73812e935ff3b20723af17abf14b0509ca37ae96
DESKTOP-CA4JH2I\User <nivek@wimtechnologies.com>
2019-05-30 12:33:44 +0200

    Request user input

--- Assigment 1/Question3.2.py
+++ Assigment 1/Question3.2.py
@@ -1,1 +1,15 @@
-print("Hello World")
+def CalculateMinEditDistance(str1,str2, insertWeight, deleteWeight,substituteWeight):
+    print(str1)
+    print(str2)
+    print(insertWeight)
+    print(deleteWeight)
+    print(substituteWeight)
+
+
+str1 = input("Please insert word 1")
+str2 = input("Please insert word 2")
+insertWeight = input("Please weighting for an insert")
+deleteWeight = input("Please weighting for a delete")
+substituteWeight = input("Please weighting for a substitute")
+CalculateMinEditDistance(str1, str2, insertWeight, deleteWeight, substituteWeight)
+
