ee3673bc8f67b86b9e866376652d0d5cbd2e8c9e
DESKTOP-CA4JH2I\User <nivek@wimtechnologies.com>
2019-08-09 09:00:59 +0200

    Modified Question 2(a) to use actual argmax probability
    Also gave option for case sensitive

--- Assignment 2/Question 2.py
+++ Assignment 2/Question 2.py
@@ -17,6 +17,10 @@ def assignTagSet(filename):
         if f.mode == 'r':
             datatowrite = ""
             contents = f.read()
+
+            if not casesensitive:
+                contents = contents.lower()
+
             spacesplit = contents.split()
             for x in spacesplit:
                 if not x.lower() in dictwordstagmap:
@@ -37,15 +41,34 @@ def assignTagSet(filename):
 
 
 def findargmax(contents):
+    if not casesensitive:
+        contents = contents.lower()
+
     spaceSplit = contents.split()
     dictWordsTagCount = {}
+    totalCountPerWord = {}
+    totalCountPerWordTag = {}
+
     for i in range(len(spaceSplit)):
+        if not spaceSplit[i] in totalCountPerWordTag:
+            totalCountPerWordTag[spaceSplit[i]] = 0
+        totalCountPerWordTag[spaceSplit[i]] += 1
+
         slashsplit = spaceSplit[i].split("/")
-        if not slashsplit[0] in dictWordsTagCount:
-            dictWordsTagCount[slashsplit[0]] ={}
-        if not slashsplit[1] in dictWordsTagCount[slashsplit[0]]:
-            dictWordsTagCount[slashsplit[0]][slashsplit[1]] = 0
-        dictWordsTagCount[slashsplit[0]][slashsplit[1]] += 1
+        if not slashsplit[0] in totalCountPerWord:
+            totalCountPerWord[slashsplit[0]] = 0
+        totalCountPerWord[slashsplit[0]] += 1
+
+    for wordtag in totalCountPerWordTag:
+        wordtagTotalCount = totalCountPerWordTag[wordtag]
+        wordtagsplit = wordtag.split("/")
+        wordTotalCount = totalCountPerWord[wordtagsplit[0]]
+
+        if not wordtagsplit[0] in dictWordsTagCount:
+            dictWordsTagCount[wordtagsplit[0]] = {}
+        if not wordtagsplit[1] in dictWordsTagCount[wordtagsplit[0]]:
+            dictWordsTagCount[wordtagsplit[0]][wordtagsplit[1]] = 0
+        dictWordsTagCount[wordtagsplit[0]][wordtagsplit[1]] = wordtagTotalCount/wordtagTotalCount
 
     for x in dictWordsTagCount:
         maxCount = -1
@@ -55,12 +78,17 @@ def findargmax(contents):
                 maxCount = dictWordsTagCount[x][tagcount]
                 tagName = tagcount
 
-        print(x,"-",tagName)
+        print(x, "-", tagName)
         dictwordstagmap[x.lower()] = tagName
 
     filename = input("\nPlease enter second filename including extension\n")
     assignTagSet(filename)
 
 dictwordstagmap ={}
+casesensitivetext = input("Case Sensitive? (Y/N) \n")
+casesensitive = False
+if casesensitivetext.lower() == "Y":
+    casesensitive = True;
+
 filename = input("Please enter training filename including extension\n")
 main(filename)
